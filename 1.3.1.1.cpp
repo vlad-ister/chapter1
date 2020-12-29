2de5a37eafc156fa6b26863588fd6cc5cbc091a1
Владислав Бочек <76567940+vlad-ister@users.noreply.github.com>
2020-12-29 18:22:07 +0200

    Add files via upload
    1.3.1.1 Comments: are they always useful?

---
+++ 1.3.1.1.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+#include <iomanip>
+#include <string>
+
+int main()
+{
+  int v=10800;
+  int z=3*60;
+  int zzz=6*60;
+  float siii=3.141526;
+  std::cout << "result:  " << v <<  std::endl;
+  std::cout << "result:  " << z <<  std::endl;
+  std::cout << "result:  " << zzz <<  std::endl;
+  std::cout << "result:  " << siii <<  std::endl;
+}

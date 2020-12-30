9ab2e97d5f2983905e933f9959c2708208050fcf
Владислав Бочек <76567940+vlad-ister@users.noreply.github.com>
2020-12-30 12:44:31 +0200

    Add files via upload

---
+++ 1.7.4.2.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <cmath>
+#include <iomanip>
+
+using namespace std;
+
+int main(){
+float x,y;
+
+cout<<"Enter the first number: ";
+cin>>x;
+cout<<"Enter the second number: ";
+cin>>y;
+
+if (1/y==1/x){
+    cout<<"The numbers: "<<x<<" and "<<y<<" are equal (by 0.000001 epsilon)";
+}
+else{
+    cout<<"The numbers: "<<x<<" and "<<y<<" are not equal (by 0.000001 epsilon)";
+    
+}
+}
